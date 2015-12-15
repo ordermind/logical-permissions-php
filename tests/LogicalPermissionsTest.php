5ccdbfca3c052fd73d53e90390ca459125082387
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2015-12-15 12:01:01 +0100

    kristofer: Added support for PHPUnit

---
+++ tests/LogicalPermissionsTest.php
@@ -0,0 +1,12 @@
+<?php
+ 
+use Ordermind\LogicalPermissions\LogicalPermissions;
+ 
+class LogicalPermissionsTest extends PHPUnit_Framework_TestCase {
+  public function testInit()
+  {
+    $lp = new LogicalPermissions;
+    $this->assertTrue($lp);
+  }
+ 
+} 
