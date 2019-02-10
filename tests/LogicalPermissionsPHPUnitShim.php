5bae8a42c4b1adbcaff953cd1d140863cd01c215
Kristofer Tengström <info@kristofer-tengstrom.se>
2019-02-10 18:22:56 +0100

    Changed access level for shim method to public

--- tests/LogicalPermissionsPHPUnitShim.php
+++ tests/LogicalPermissionsPHPUnitShim.php
@@ -5,7 +5,7 @@ namespace Ordermind\LogicalPermissions\Test;
 // Solves issue with different parent classes in different versions of PHPUnits. Each test class should extend this.
 if(class_exists('PHPUnit_Framework_TestCase')) {
   class LogicalPermissionsPHPUnitShim extends \PHPUnit_Framework_TestCase {
-    protected function expectException($class) {
+    public function expectException($class) {
       $this->setExpectedException($class);
     }
   }
